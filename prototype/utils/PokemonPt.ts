208f3165d1c4d43e4740bbf1823faa5567dd985f
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2024-12-02 20:32:43 -0300

    ✨ Adicionar método clonar nas classes Selvagem e Domestico

---
+++ prototype/utils/PokemonPt.ts
@@ -0,0 +1,72 @@
+import { IPokemon } from './IPokemon';
+
+export abstract class Pokemon implements IPokemon {
+    clonar(): Pokemon {
+        throw new Error('Method not implemented.');
+    }
+    private _nome: string;
+    private _energia: number;
+    private _ataque: number;
+    private _defesa: number;
+
+    constructor(
+        nome: string,
+        energia: number,
+        ataque: number,
+        defesa: number
+    ) {
+        this._nome = nome;
+        this._energia = energia;
+        this._ataque = ataque;
+        this._defesa = defesa;
+    }
+
+    public get nome(): string {
+        return this._nome;
+    }
+
+    public get energia(): number {
+        return this._energia;
+    }
+
+    public get ataque(): number {
+        return this._ataque;
+    }
+
+    public get defesa(): number {
+        return this._defesa;
+    }
+
+    public abstract atacar(): string;
+    public abstract defender(): string;
+    public abstract status(): string;
+
+    public treinarAtaque(): void {
+        this._ataque += this.random(7);
+        this._energia -= this.random(10);
+        if (this._ataque > 100) this._ataque = 100;
+    }
+
+    public treinarDefesa(): void {
+        this._defesa += this.random(3);
+        this._energia -= this.random(10);
+        if (this._defesa > 100) this._defesa = 100;
+    }
+
+    public batalhar(): void {
+        this._energia -= this.random(10);
+    }
+
+    public descansar(horas: number): void {
+        this._energia += horas * this.random(5);
+        if (this._energia > 100) this._energia = 100;
+    }
+
+    public energiaAcabou(): boolean {
+        return this._energia <= 0;
+    }
+
+    private random(fator: number): number {
+        return Math.floor(Math.random() * fator) + 1;
+    }
+}
---
+++ prototype/utils/SelvagemPt.ts
@@ -0,0 +1,46 @@
+import { Pokemon } from "./PokemonPt";
+import { Clonavel } from "./clonavel";
+
+export class Selvagem extends Pokemon {
+  private _regiao: string;
+
+  constructor(
+    regiao: string,
+    nome: string,
+    energia: number,
+    ataque: number,
+    defesa: number
+  ) {
+    super(nome, energia, ataque, defesa);
+    this._regiao = regiao;
+  }
+
+  public atacar(): string {
+    return "Ataque Selvagem";
+  }
+
+  public defender(): string {
+    return "Defesa Selvagem";
+  }
+
+  public status(): string {
+    return `
+            Status Atual:
+            Pokémon: ${this.nome}
+            Energia: ${this.energia}
+            Ataque: ${this.ataque}
+            Defesa: ${this.defesa}
+            Região: ${this._regiao}
+        `;
+  }
+
+  public clonar(): Pokemon {
+    return new Selvagem(
+      this._regiao,
+      this.nome,
+      this.energia,
+      this.ataque,
+      this.defesa
+    );
+  }
+}
